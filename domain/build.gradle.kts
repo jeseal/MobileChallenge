14e027aee6fe277e717bab34d6e70305d56a0fb7
Jese Alvarado <ppealvarado@gmail.com>
2023-11-21 15:18:53 -0600

    🎨 feat: Clean domain module dependencies

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -1,43 +1,45 @@
 plugins {
-    id("com.android.library")
-    id("org.jetbrains.kotlin.android")
+    id(GradlePlugings.androidLibrary)
+    id(GradlePlugings.kotlinAndroid)
 }
 
 android {
-    namespace = "com.example.domain"
-    compileSdk = 34
+    namespace = "${ProjectConfiguration.applicationId}.${project.name}"
+    println("namespace: ${namespace.orEmpty()}")
+    compileSdk = ProjectConfiguration.compileSdk
 
     defaultConfig {
-        minSdk = 24
+        minSdk = ProjectConfiguration.minSdk
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
     }
 
     buildTypes {
-        release {
+        getByName("release") {
             isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+        getByName("debug") {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
 }
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.10.0")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation(Deps.Core.coreKtx)
+    //implementation("androidx.appcompat:appcompat:1.6.1")
+    //implementation("com.google.android.material:material:1.10.0")
+    testImplementation(Deps.Core.jUnit)
+    androidTestImplementation(Deps.Core.androidxJunit)
+    androidTestImplementation(Deps.Core.espresso)
 }
