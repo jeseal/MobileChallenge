d88815bef4ebbd03125b3bc28e701a0cda3a118d
Jese Alvarado <ppealvarado@gmail.com>
2023-11-21 14:50:42 -0600

    🎨 feat: Clean data module sdk

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -5,31 +5,34 @@ plugins {
 }
 
 android {
-    namespace = "com.example.data"
-    compileSdk = 34
+    namespace = "${ProjectConfiguration.applicationId}.${project.name}"
+    println("namespace: ${namespace.orEmpty()}")
+    compileSdk = ProjectConfiguration.compileSdk
 
     defaultConfig {
-        minSdk = 24
+        minSdk = ProjectConfiguration.minSdk
+        targetSdk = ProjectConfiguration.targetSdk
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
     }
 
     buildTypes {
-        release {
+        getByName("release") {
             isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+        getByName("debug") {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
 }
 
