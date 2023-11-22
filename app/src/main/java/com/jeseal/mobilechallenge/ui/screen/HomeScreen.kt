93015fa33fc02afd3758e7bb10cce85476d57d17
Jese Alvarado <ppealvarado@gmail.com>
2023-11-22 10:19:13 -0600

    💄 feat: Use coil to show image character

--- app/src/main/java/com/jeseal/mobilechallenge/ui/screen/HomeScreen.kt
+++ app/src/main/java/com/jeseal/mobilechallenge/ui/screen/HomeScreen.kt
@@ -1,5 +1,6 @@
 package com.jeseal.mobilechallenge.ui.screen
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -7,17 +8,24 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil.compose.rememberAsyncImagePainter
+import coil.compose.rememberImagePainter
 import com.jeseal.domain.model.Character
 
 @Composable
@@ -35,7 +43,7 @@ fun CharactersScreen(
                 modifier = Modifier.fillMaxSize()
             ) {
                 items(state.characters) { character ->
-                    CountryItem(
+                    CharacterItem(
                         character = character,
                         modifier = Modifier
                             .fillMaxWidth()
@@ -50,7 +58,7 @@ fun CharactersScreen(
 }
 
 @Composable
-private fun CountryItem(
+private fun CharacterItem(
     character: Character?,
     modifier: Modifier = Modifier
 ) {
@@ -58,24 +66,19 @@ private fun CountryItem(
         modifier = modifier,
         verticalAlignment = Alignment.CenterVertically
     ) {
-        character?.name?.let {
-            Text(
-                text = it,
-                fontSize = 30.sp
-            )
-        }
-        Spacer(modifier = Modifier.width(16.dp))
+        Image(
+            painter = rememberAsyncImagePainter(character?.image),
+            contentDescription = "Character Image",
+            modifier = Modifier
+                .size(80.dp)
+                .clip(MaterialTheme.shapes.medium),
+            contentScale = ContentScale.Crop,
+        )
+        Spacer(modifier = Modifier.height(16.dp))
         Column(
-            modifier = Modifier.weight(1f)
+            modifier = Modifier.weight(1f).padding(16.dp)
         ) {
-            character?.id?.let {
-                Text(
-                    text = it,
-                    fontSize = 24.sp
-                )
-            }
-            Spacer(modifier = Modifier.width(16.dp))
-            character?.species?.let { Text(text = it) }
+            character?.name?.let { Text(text = it, style = MaterialTheme.typography.headlineSmall) }
         }
     }
 }
