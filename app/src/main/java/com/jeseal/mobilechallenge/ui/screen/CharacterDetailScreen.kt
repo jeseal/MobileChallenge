88868d8a3bc8cc029f85261de20fd9d9b8daa0dc
Jese Alvarado <ppealvarado@gmail.com>
2023-11-23 02:57:58 -0600

    💄 feat: Add character detail

--- app/src/main/java/com/jeseal/mobilechallenge/ui/screen/CharacterDetailScreen.kt
+++ app/src/main/java/com/jeseal/mobilechallenge/ui/screen/CharacterDetailScreen.kt
@@ -1,17 +1,17 @@
 package com.jeseal.mobilechallenge.ui.screen
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberAsyncImagePainter
 
@@ -19,37 +19,56 @@ import coil.compose.rememberAsyncImagePainter
 fun CharacterDetailScreen(
     state: HomeViewModel.CharactersState
 ) {
-    Column(
-        modifier = Modifier.fillMaxWidth(),
-        verticalArrangement = Arrangement.SpaceBetween
+    Card(
+        modifier = Modifier.fillMaxSize()
+            .padding(8.dp)
     ) {
-        Column(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.spacedBy(8.dp)
-        ) {
-            Spacer(modifier = Modifier.height(16.dp))
+        Column {
             Image(
                 painter = rememberAsyncImagePainter(state.selectedCharacter?.image),
-                contentDescription = null,
-                modifier = Modifier.width(300.dp)
+                contentDescription = "Character Image",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(194.dp),
+                contentScale = ContentScale.Crop,
             )
-            Spacer(modifier = Modifier.height(16.dp))
             state.selectedCharacter?.name?.let {
                 Text(
                     text = it,
                     style = MaterialTheme.typography.headlineSmall
                 )
             }
-           // Spacer(modifier = Modifier.height(8.dp))
             state.selectedCharacter?.species?.let {
                 Text(
                     text = it,
                     style = MaterialTheme.typography.headlineSmall
                 )
             }
-            Spacer(modifier = Modifier.height(8.dp))
             state.selectedCharacter?.location?.let {
+                Text(
+                    text = it,
+                    style = MaterialTheme.typography.headlineSmall
+                )
+            }
+            state.selectedCharacter?.gender?.let {
+                Text(
+                    text = it,
+                    style = MaterialTheme.typography.headlineSmall
+                )
+            }
+            state.selectedCharacter?.status?.let {
+                Text(
+                    text = it,
+                    style = MaterialTheme.typography.headlineSmall
+                )
+            }
+            state.selectedCharacter?.origin?.let {
+                Text(
+                    text = it,
+                    style = MaterialTheme.typography.headlineSmall
+                )
+            }
+            state.selectedCharacter?.type?.let {
                 Text(
                     text = it,
                     style = MaterialTheme.typography.headlineSmall
